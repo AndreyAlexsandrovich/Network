5b97329f0f701af778dc3f271b901a679517a80e
Adnrey <lomovskoy.andrey@bk.ru>
2025-09-03 21:18:26 +0300

    add api products

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,5 +1,4 @@
 export default function apiProducts() {
-  const containerProduct = document.querySelector('.product');
   const place = document.querySelector('#places__list');
 
   fetch('https://api.escuelajs.co/api/v1/products')
@@ -11,33 +10,15 @@ export default function apiProducts() {
   })
   .then(json => {
     json.forEach((item) => { 
-      const {id, title, price, description, images } = item;
-      place.innerHTML = ``
+      const { title, price, description, images } = item;
+       const templateProduct = document.querySelector('#template-product').content.cloneNode(true);
+       templateProduct.querySelector('#title-product').textContent = title;
+       templateProduct.querySelector('#price-product').textContent = `${price + '$'}`;
+       templateProduct.querySelector('#description').textContent = description;
+       templateProduct.querySelector('#image-prduct').src = images[0];
+      
 
-      const itemHTML = `
-       <li class="product" id=${id}>
-          <div class="product-container">
-            <div class="title-wrapper">
-              <h2 class="title-product" id="title-product">${title}</h2>
-            </div>
-            <div class="image-product-wrapper">
-              <img src="${images[1]}"
-              alt="Обложка товара" class="image-product">
-            </div>
-            <div class="description-product-wrapper">
-                <p class="description-product" id="description">${description}</p>
-                </div>
-            <div class="price-product">
-              <p class="price" id="price-product">${price}$</p>
-            </div>
-            <div class="buy-product">
-              <button class="buy">Купить товар</button>
-            </div>
-          </div>
-        </li>
-      `
-
-      place.insertAdjacentHTML('beforeend', itemHTML);
+       place.appendChild(templateProduct);
     })
   })
   .catch(error => console.log('Ошибка:', error));
